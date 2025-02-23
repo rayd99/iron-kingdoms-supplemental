1a346e42eef3abf1db4ab354a1e90bdfd590dd51
Rayd99 <raerlynn@gmail.com>
2025-02-22 23:36:52 -0600

    Experimenting with deleting a top level category. Also moved kingdom dialects under standard languages.

--- world-script.js
+++ world-script.js
@@ -3,11 +3,13 @@ Hooks.once("init", () => {
 // Deleting languages that don't fit this world
     delete CONFIG.DND5E.languages.druidic;
     delete CONFIG.DND5E.languages.exotic.children.aarakocra;
-    delete CONFIG.DND5E.languages.exotic.children.primordial.children.aquan;    
+    delete CONFIG.DND5E.languages.exotic.children.primordial;    
     
 // Adding Kingdom Languages
-    CONFIG.DND5E.languages.kingdomdialects = "Kingdom Dialects";
-    CONFIG.DND5E.languages.kingdomdialects.children.cygnaran = "Cygnaran";
-    CONFIG.DND5E.languages.kingdomdialects.children.khadorn = "Khadoran";
-
+    CONFIG.DND5E.languages.standard.kingdomdialects = "Kingdom Dialects";
+    CONFIG.DND5E.languages.standard.kingdomdialects.children.cygnaran = "Cygnaran";
+    CONFIG.DND5E.languages.standard.kingdomdialects.children.khadorn = "Khadoran";
+    CONFIG.DND5E.languages.standard.kingdomdialects.children.khadorn = "Menite";
+    CONFIG.DND5E.languages.standard.kingdomdialects.children.khadorn = "Llaelese";
+    CONFIG.DND5E.languages.standard.kingdomdialects.children.khadorn = "Rhulic";
   });
