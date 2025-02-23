0857b56630aa08fad78f3b6c32fffbfb84cf6e3b
Rayd99 <raerlynn@gmail.com>
2025-02-23 00:13:28 -0600

    Adjusted languages and removed unneeded languages.

--- world-script.js
+++ world-script.js
@@ -1,19 +1,33 @@
 Hooks.once("init", () => {
 // Deleting languages that don't fit this world
+    delete CONFIG.DND5E.languages.standard.elvish;
+    delete CONFIG.DND5E.languages.standard.giant;
+    delete CONFIG.DND5E.languages.standard.gnomish;
+    delete CONFIG.DND5E.languages.standard.goblin;
+    delete CONFIG.DND5E.languages.standard.halfling;
+    delete CONFIG.DND5E.languages.standard.orc;
+
+// Deleting exotic languages that don't fit this world
     delete CONFIG.DND5E.languages.druidic;
     delete CONFIG.DND5E.languages.exotic.children.aarakocra;
     delete CONFIG.DND5E.languages.exotic.children.primordial;    
+    delete CONFIG.DND5E.languages.exotic.children.deepspeech;
+    delete CONFIG.DND5E.languages.exotic.children.gith;
+    delete CONFIG.DND5E.languages.exotic.children.gnoll;
+    delete CONFIG.DND5E.languages.exotic.children.sylvan;
     
-// Adding Kingdom Languages
-CONFIG.DND5E.languages.standard.children.gibberish = {
-    label: "Kingdom Dialects",
-    children: {
-      cygnaran: "Cygnaran",
-      khadoran: "Khadoran",
-      menite: "Menite",
-      llaelese: "Llaelese",
-      rhulic: "Rhulic"
-    }
-  };
-    
+// Adding Iron Kingdom Languages
+    CONFIG.DND5E.languages.standard.children.cygnaran = "Cygnaran";
+    CONFIG.DND5E.languages.standard.children.shyr = "Shyr";
+    CONFIG.DND5E.languages.standard.children.gobberish = "Gobberish";
+    CONFIG.DND5E.languages.standard.children.khadoran = "Khadoran";
+    CONFIG.DND5E.languages.standard.children.llaelese = "Llaelese";
+    CONFIG.DND5E.languages.standard.children.menite = "Menite";
+    CONFIG.DND5E.languages.standard.children.molgurog = "Molgur-Og";
+    CONFIG.DND5E.languages.standard.children.molgurtrul = "Molgur-Trul";
+    CONFIG.DND5E.languages.standard.children.ordic = "Ordic";
+    CONFIG.DND5E.languages.standard.children.scharde = "Scharde";
+
+// Adding rare languages
+CONFIG.DND5E.languages.exotic.children.seeker = "Seeker";
   });
